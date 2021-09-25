88518cc027bc38ca44d03ae089821f9036905e99
Nikos Chantziaras <realnc@gmail.com>
2021-09-25 15:04:03 +0300

    Bump hugo-unix to 3.1.06

--- Formula/hugo-unix.rb
+++ Formula/hugo-unix.rb
@@ -1,8 +1,10 @@
 class HugoUnix < Formula
   desc "Hugo command-line compiler, debugger and interpreter"
   homepage "https://www.generalcoffee.com/hugo"
-  url "https://github.com/0branch/hugo-unix/releases/download/v3.1.05/hugo-unix-3.1.05-source.tar.bz2"
-  sha256 "e06e7b063c9fc3458dbee1860751489fafc2dcd755e0f443ae42b7971ec80872"
+  url "https://github.com/hugoif/hugo-unix/releases/download/v3.1.06/hugo-unix-3.1.06-source.tar.bz2"
+  sha256 "429eac590a08645b9b91b4b77458702eec9d7f0aff4ae647662a50e0e95e4b54"
+  license all_of: ["BSD-2-Clause", "LGPL-3.0-or-later"]
+  head "https://github.com/hugoif/hugo-unix.git"
 
   depends_on "pkg-config" => :build
   depends_on "fluid-synth"
